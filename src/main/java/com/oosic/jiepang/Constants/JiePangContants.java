af61504787a5db7b17744a11a3cc66a963273a7e
feng_xiang <wow2009zfx@gmail.com>
2013-07-03 09:44:07 +0800

    init

---
+++ src/main/java/com/oosic/jiepang/Constants/JiePangContants.java
@@ -0,0 +1,13 @@
+package com.oosic.jiepang.Constants;
+
+public class JiePangContants {
+	public static final String APPID = "100989";
+	public static final String APPSECRET = "a9875016647fc6f64d785bb1684374fc";
+	
+	
+	//Implicit Grant
+	public static final String OAUTH_PATH = "https://jiepang.com/oauth/authorize";
+	
+	
+	public static final String LOCATION_SHOW_PATH = "http://api.jiepang.com/v1/locations/show";
+}
